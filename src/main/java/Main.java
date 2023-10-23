a60f8d396ec23ca92099348a31b290419a0ba70e
MattiaBiga200701 <mattia.biga@students.uniroma2.eu>
2023-10-23 19:16:43 +0200

    UPDATE

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ public class Main {
     public static void main(String[] args){
         Controller controllerClass = new Controller();
         System.out.println("[JAVA PRINT] Main class started;\n");
-        System.out.println("commit");
         controllerClass.initializer();
 
 
